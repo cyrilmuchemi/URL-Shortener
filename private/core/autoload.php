aeb113e1614a6ed58866c86be31af0729406407c
cyrilmuchemi <cyril.muchemi@gmail.com>
2024-04-15 12:43:08 +0300

    Settup frontend and backend

---
+++ private/core/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+require "app.php";
+require "config.php";
+require "database.php";
---
+++ public/frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <h1>URL Shortener</h1>
+</template>
+
+<style scoped>
+</style>
